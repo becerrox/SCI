65c949fe1b66d0ced4abeb2e4adce201dd430b8d
luisrlavado <luisrlavado@gmail.com>
2016-03-15 10:30:34 -0430

    configuracion de bienes

--- js/Login.js
+++ js/Login.js
@@ -24,10 +24,35 @@ $(document).ready(function(){
 });
 
 
-//Servicio de listar equipos
+//Servicio de Catalogo de bienes
 
 $(document).ready(function(){
-	$("#Registrar").click(function(){
+	$("#RegistrarCatalogo").click(function(){
+		var Codigo=$('#codigo').val();
+		var Descripcion=$("#descripcion").val();
+		var Tipo=$('#tipo').val();
+		//Location='registrousuario.html';
+		console.log(Codigo,Descripcion,Tipo);
+		$.ajax({
+			type:"POST",
+			processData: false,
+    		contentType: 'application/json',
+			url:'api/services/token',
+			data:JSON.stringify({codigo:Codigo,descripcion:Descripcion,tipo:Tipo}),
+			success:function(response){
+				location.href='inicio.html';
+			},error:function(response){
+				console.log(response);
+				$("#mensaje").html(response.responseJSON.data.error);
+			}
+		});
+	});
+});
+
+//Servicio de lis
+
+$(document).ready(function(){
+	$("#RegistrarEquipo").click(function(){
 		var Codigo=$('#codigo').val();
 		var Descripcion=$("#descripcion").val();
 		var Tipo=$('#tipo').val();
