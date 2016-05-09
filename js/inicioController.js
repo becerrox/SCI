784b861d7e157b502d4d4dcafa435b9adeb4ff6c
corneliz <cornelizyoselin@gmail.com>
2016-05-08 21:45:47 -0430

    Permisos por Usuario

--- js/inicioController.js
+++ js/inicioController.js
@@ -17,5 +17,24 @@ $(document).ready(function()
                 sessionStorage.removeItem("dataUsuario");
                 location.href="index.html";    
              });
+
+        if (dataUsuario.nivel==1) //Nivel Administrador de Bienes
+            {
+                document.getElementById("equipos").style.display = "none";
+                document.getElementById("reporte_equipos").style.display = "none";     
+            }
+        if (dataUsuario.nivel==2) //Nivel Regular Usuario de Bienes
+            {
+                document.getElementById("equipos").style.display = "none";
+                document.getElementById("reporte_equipos").style.display = "none";
+                document.getElementById("registro_usuarios").style.display = "none";
+                document.getElementById("reporte_equipos").style.display = "none";
+                 document.getElementById("btnEliminar").style.display = "none";
+
+            }        
+
+
+
+
         });        
 });
