44b015235805f7d0fdc99cfb290f0f75605a1660
luis lavado <luis.lavado16@gmail.com>
2016-06-06 19:12:42 -0400

    Contancia

--- js/constanciaRetiroController.js
+++ js/constanciaRetiroController.js
@@ -10,19 +10,6 @@ $(document).ready(function()
 
     var id_equipo_editar = "";
 
-        $("#buscarEquipo").click(function(){
-            idEquipo = $
-            numero = $("#serial").val();
-            getEquiposBy("?serial="+numero).then(function(data){
-                id_equipo_editar = data[0].id;
-                jsonToTableTraspuesto({
-                    data : data,
-                    headers : headers,
-                    table : $("#tableEquipos"),
-                    invisibleFields : ["id","color","unidad_admin","status","responsable","fecha_modif","unidad_trabajo","serialSim","accesorios","planCelular","numeroCelular","motivo"]
-                  });              
-            });
-        });  
 
         $("#buscarCelular").click(function(){
             idEquipo = $
@@ -49,8 +36,7 @@ $(document).ready(function()
             });
         }); 
 
-        $("#btnGuardar").click(function()
-        {
+        $("#btnGuardar").click(function(){
             var responsb = document.getElementById("nombres").value.toUpperCase() + " " + document.getElementById("apellidos").value.toUpperCase();
 
             frm = $("#constanciaRetiro");
@@ -66,7 +52,6 @@ headers = [];
 
 
 
-
         /* Cargando datas de la api a los selects  */           
   
 
@@ -75,7 +60,7 @@ headers = [];
                 data[personal].nombreApellido = data[personal].nombres + " "+data[personal].apellidos;
                 data[personal].ci = data[personal].cedula;
             }
-            jsonToSelect({        
+            jsonToSelect({
                  data : data,
                  value : "ci",
                  alias : "nombreApellido",
@@ -85,9 +70,9 @@ headers = [];
                     data : data,
                     form : "#constancia"
                 });                 
-         })  
+         })
+});
 
-        //Descripción
         getConfiguracionEquipoBy("?tipo=DESCRIPCIÓN&status=1").then(function(data){
             jsonToSelect({        
                 data : data,
@@ -95,8 +80,61 @@ headers = [];
                 alias : "descripcion",
                 element : $("#descripcion")
                 });
-        })    
+        })
+        
+        var equipos = "";
+        var modelos = "";
+
+        function cargarSelectEquipos(){
+            equipos = document.getElementById("descripcion").value;
+            getEquiposBy("?descripcion="+equipos+"&status=1").then(function(data){
+                jsonToSelect({
+                    data : data,
+                    value : "modelo",
+                    alias : "modelo",
+                    element : $("#modelo")
+                    });
+            })
+        }
+
+        function cargarSelectModelo(){
+            modelos = document.getElementById("modelo").value;
+            getEquiposBy("?descripcion="+equipos+"&modelo="+modelos+"&status=1").then(function(data){
+                jsonToSelect({
+                    data : data,
+                    value : "serial",
+                    alias : "serial",
+                    element : $("#serial")
+                    });
+            })
+        }
+
+        function buscarEquipo(){
+            idEquipo = $
+            numero = $("#serial").val();
+            getEquiposBy("?serial="+numero).then(function(data){
+                id_equipo_editar = data[0].id;
+                console.log(data[0].descripcion);
+                if (data[0].descripcion == "CELULAR")
+                {
+                jsonToTableTraspuesto({
+                    data : data,
+                    headers : headers,
+                    table : $("#tableEquipos"),
+                    invisibleFields : ["id","estado","unidad_admin","status","responsable","fecha_modif","unidad_trabajo","estadoUsoEquipo","motivo"]
+                  });              
+                }
+                else
+                jsonToTableTraspuesto({
+                    data : data,
+                    headers : headers,
+                    table : $("#tableEquipos"),
+                    invisibleFields : ["id","color","unidad_admin","status","responsable","fecha_modif","unidad_trabajo","serialSim","accesorios","planCelular","numeroCelular","motivo","serialTelefono"]
+                  });                    
+            });
+        }  
 
 
-});
+
+
 
